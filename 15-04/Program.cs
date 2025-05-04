1ecb57c8f00f56cd9509bb9855b6ed65a3199ef0
vagner jorge <svagnerjorge2@gmail.com>
2025-05-04 13:13:15 -0300

    cx

---
+++ 15-04/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string[] fila = new string[10];
+            int fimfila = 0;
+       
+
+            do
+            {
+                Console.WriteLine("1-cadastrar cliente");
+                Console.WriteLine("2-listar fila");
+                Console.WriteLine("3-cadastrar c. prioritario");
+                Console.WriteLine("4-atender cliente");
+                Console.WriteLine("q-sair");
+              
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+    Console.WriteLine("-cadastrar cliente-");
+                        Console.WriteLine("");
+                      
+                        if ( fimfila < 10)
+                        {
+                            Console.WriteLine("nome do cliente");
+                      string nome = Console.ReadLine();
+                            fila[fimfila++] = nome;
+                        }
+                        else
+                        {
+                            Console.WriteLine("fila cheia , retorne outra hora");
+                        }
+                        break;
+                      
+                    
+                    
+                    case"2":
+                        Console.WriteLine("-listando fila-");
+                        for (int i = 0; i < fimfila ;i++) {
+                         
+                            Console.WriteLine($"{ fila[i]}");
+                        }
+                            break;
+
+
+
+                        case "3":
+                         Console.WriteLine("-cadastrar c. prioritario-");
+                        Console.WriteLine("");
+
+                        if (fimfila <= 10)
+                        {
+
+                         
+                            for (int i = fimfila; i > 0; i--)
+                            {
+                                fila[i] = fila[i - 1];
+                            }
+                            
+                            Console.WriteLine("nome do cliente");
+                            string nome = Console.ReadLine();
+                            fila[0] = nome;
+                            fimfila++;
+                        }
+                        else
+                        {
+                           
+                            for (int i = fimfila; i > 0 ; i--)
+                            {
+                                fila[i] = fila[i - 1];
+                            }
+                           
+                            fila[fimfila - 1] = null;
+                            Console.WriteLine("nome do cliente");
+                            string nome = Console.ReadLine();
+                            fila[0] = nome;
+                            fimfila++;
+                        }
+                            break;
+                        
+                    
+                    
+                    case"4":
+
+
+                        for (int i = 0; i <= fimfila - 1; i++)
+                        {
+                         
+                            fila[i] = fila[i + 1];
+                        }
+                        fimfila--;
+                   
+
+                        Console.WriteLine($"ateder cliente {fila[0]}");
+                        break;
+                }
+            } while (input !="q");
+
+
+
+
+
+        }
+    }
+}
---
+++ 15-04/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("15-04")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("15-04")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("0fab10ef-571f-4ab3-8599-335f926aac36")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
